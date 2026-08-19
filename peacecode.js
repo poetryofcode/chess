47579935c0b3ecee885c80a0a371adaa0cbe806d
VitaliPri <vitaliy.prymak@cix.csi.cuny.edu>
2026-08-19 19:10:38 -0400

    daliy dose

--- peacecode.js
+++ peacecode.js
@@ -402,8 +402,6 @@ function filter_list(l) {
 var firstName = { A: "Alpha", B: "Beta", C: "Cache" };
 var surname = { A: "Analogue", B: "Bomb", C: "Catalyst" };
 
-function aliasGen(first, last) {
-  // Code Here
-}
+function aliasGen(first, last) {}
 const result = aliasGen(first, last);
 console.log(result);
